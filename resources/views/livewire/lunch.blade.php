76cddc9bc26c00a82634e32ccfd08eda58b9a630
SaddamParikesitIbnuYahsi <saddampk124@gmail.com>
2024-12-05 16:28:24 +0700

    akhir commit

--- resources/views/livewire/lunch.blade.php
+++ resources/views/livewire/lunch.blade.php
@@ -4,9 +4,7 @@
     <div class="bg-gray-800 rounded-lg p-4 flex flex-col items-center shadow-lg">
         <img src="{{ asset('storage/' . $item['foto']) }}" alt="{{ $item['menu'] }}" class="rounded-lg w-full h-32 object-cover mb-4">
         <h3 class="text-lg font-semibold text-white">{{ $item['menu'] }}</h3>
-        <h4>Bahan</h4>
         <h2 class="text-2xl font-semibold mb-4">{{ $item['bahan']}}</h2>
-        <h4>Cara Membuat</h4>
         <h2 class="text-2xl font-semibold mb-4">{{ $item['cara']}}</h2>
         <button class="mt-4"><a href="oatmeal" class="bg-indigo-600 text-white px-4 py-2 mt-2 rounded-lg hover:bg-indigo-700 transition">Lihat</a></button>
     </div>
